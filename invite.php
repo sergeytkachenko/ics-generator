33a80dd1589d3f14c371b8afbd2426c4d9e07e76
Ahmad Amin <AhmadFAmin@gmail.com>
2012-04-12 15:14:14 -0400

    Updated Format

--- invite.php
+++ invite.php
@@ -14,7 +14,15 @@ class Invite
 	return $this;
     }
 
-    public function setDate($start, $end)
+    /**
+     * 
+     * Set the event stat and end time.
+     * 
+     * @param DateTime $start
+     * @param DateTime $end
+     * @return \Invite 
+     */
+    public function setDate(DateTime $start, DateTime $end)
     {
 	$this->setStartDate($start);
 	$this->setEndDate($end);
@@ -22,18 +30,36 @@ class Invite
 	return $this;
     }
 
-    public function setStart($start)
+    /**
+     * Set the start datetime
+     * @param DateTime $start
+     * @return \Invite 
+     */
+    public function setStart(DateTime $start)
     {
 	$this->_start = $start;
-
 	return $this;
     }
 
-    public function setEnd($end)
+    /**
+     * Set the end datetime
+     * @param DateTime $end
+     * @return \Invite
+     */
+    public function setEnd(DateTime $end)
     {
-	return $this->_end = $end;
+	$this->_end = $end;
+	return $this;
     }
 
+    /**
+     * 
+     * Set the of the even sender
+     * 
+     * @param string $email
+     * @param string $name
+     * @return \Invite 
+     */
     public function setFrom($email, $name = null)
     {
 	if (null === $name) {
@@ -46,6 +72,13 @@ class Invite
 	return $this;
     }
 
+    /**
+     * 
+     * Set the invite body content
+     * 
+     * @param string $body
+     * @return \Invite 
+     */
     public function setBody($body)
     {
 	$this->_body = $body;
@@ -53,31 +86,55 @@ class Invite
 	return $this;
     }
 
+    /**
+     * Get the current body content
+     * @return string
+     */
     public function getBody()
     {
 	return $this->_body;
     }
 
+    /**
+     * Just to do it.
+     * @return \Invite 
+     */
     public function __toString()
     {
 	return $this;
     }
 
+    /**
+     * Get the name of the invite sender
+     * @return string
+     */
     public function getFromName()
     {
 	return $this->_fromName;
     }
 
+    /**
+     * Get the email where the email will be sent from
+     * @return string
+     */
     public function getFromEmail()
     {
 	return $this->_fromEmail;
     }
 
+    /**
+     * Get the start time set for the even
+     * @return string
+     */
     public function getStart()
     {
 	return $this->_start;
     }
 
+    /**
+     * Get the end time set for the event
+     * @return string
+     */
     public function getEnd()
     {
 	return $this->_start;
