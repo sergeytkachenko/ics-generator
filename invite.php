436727aa29b80e33a60d7ded468fd3c9d516c377
Ahmad Amin <AhmadFAmin@gmail.com>
2012-04-12 15:07:38 -0400

    Updated Format

--- invite.php
+++ invite.php
@@ -1,82 +1,88 @@
 <?php
 
-	class Invite 
-	{
+class Invite
+{
 
-		private $_start;
+    private $_start;
+    private $_end;
+    private $_fromName;
+    private $_fromEmail;
+    private $_body;
 
-		private $_end;
+    public function __construct()
+    {
+	return $this;
+    }
 
-		private $_fromName;
+    public function setDate($start, $end)
+    {
+	$this->setStartDate($start);
+	$this->setEndDate($end);
 
-		private $_fromEmail;
+	return $this;
+    }
 
-		private $_body;
+    public function setStart($start)
+    {
+	$this->_start = $start;
 
-		public function __construct(){
-			return $this;
-		}
+	return $this;
+    }
 
-		public function setDate($start, $end){
-			$this -> setStartDate($start);
-			$this -> setEndDate($end);
+    public function setEnd($end)
+    {
+	return $this->_end = $end;
+    }
 
-			return $this;
-		}
-
-		public function setStart($start){
-			$this -> _start = $start;
-
-			return $this;
-		}
-
-		public function setEnd($end){
-			return $this -> _end = $end;
-		}
-
-		public function setFrom($email, $name = null){
-			if(null === $name){
-				$name = $email;
-			}
-
-			$this -> _fromEmail = $email;
-			$this -> _fromName  = $name;
-
-			return $this;
-		}
-
-		public function setBody($body){
-			$this -> _body = $body;
-
-			return $this;
-		}
-
-		public function getBody(){
-			return $this -> _body;
-		}
-
-		public function __toString(){
-			return $this;
-		}
-
-
-		public function getFromName(){
-			return $this -> _fromName;
-		}
-
-		public function getFromEmail(){
-			return $this -> _fromEmail;
-		}
-
-		public function getStart(){
-			return $this -> _start;
-		}
-
-		public function getEnd(){
-			return $this -> _start;
-		}
-
+    public function setFrom($email, $name = null)
+    {
+	if (null === $name) {
+	    $name = $email;
 	}
 
+	$this->_fromEmail = $email;
+	$this->_fromName = $name;
+
+	return $this;
+    }
+
+    public function setBody($body)
+    {
+	$this->_body = $body;
+
+	return $this;
+    }
+
+    public function getBody()
+    {
+	return $this->_body;
+    }
+
+    public function __toString()
+    {
+	return $this;
+    }
+
+    public function getFromName()
+    {
+	return $this->_fromName;
+    }
+
+    public function getFromEmail()
+    {
+	return $this->_fromEmail;
+    }
+
+    public function getStart()
+    {
+	return $this->_start;
+    }
+
+    public function getEnd()
+    {
+	return $this->_start;
+    }
+
+}
 
 ?>
