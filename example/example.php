1371e844716e0af8bd439703ce261dfebf0cd2f2
Ahmad Amin <AhmadFAmin@gmail.com>
2012-04-16 10:39:37 -0400

    Reset example

--- example/example.php
+++ example/example.php
@@ -10,13 +10,9 @@ $invite
 	->setEnd(new DateTime('2013-03-16 11:59PM EST'))
 	->setLocation("Queens, New York")
 	->setOrganizer("john@doe.com", "John Doe")
-	->addAttendee("ahmad@ahmadamin.com", "Ahmad Amin")
-	->generate()
-	->save();
+	->addAttendee("ahmad@ahmadamin.com", "Ahmad Amin");
 
-//Invite::getSavedPath();
-
-//$invite->download();
+$invite->download();
 
 
 // To check and see if the invite has been downloaded use the code below
