a6bc00b12efc4935d9e22b191bf356f0685d0b40
sergey <serg.tkachenko@hotmail.com>
2017-02-15 21:54:05 +0200

    added REFRESH-INTERVAL;VALUE=DURATION:P1W

--- src/IcsGenerator/Invite.php
+++ src/IcsGenerator/Invite.php
@@ -622,7 +622,8 @@ class Invite {
 		$content .= "X-WR-CALNAME;CHARSET=utf-8:" . self::$_calendarName . "\r\n";
 		$content .= "X-ORIGINAL-URL:" . self::$_originUrl . "\r\n";
 		$content .= "CALSCALE:GREGORIAN\r\n";
-		$content .= "REFRESH-INTERVAL;VALUE=DURATION:P1D\r\n";
+		$content .= "REFRESH-INTERVAL;VALUE=DURATION:PT12H\r\n";
+		$content .= "X-PUBLISHED-TTL:PT12H\r\n";
 		$content .= "METHOD:PUBLISH\r\n"; // will ask in which calendar (at least on apple calendar)
 		$content .= "BEGIN:VTIMEZONE
 TZID:Europe/Kiev
